e1a0306d58f0cf9ea24be234ca8c444600924af8
angrybirds888 <ash06scott@gmail.com>
2022-04-18 18:30:46 +0500

    Created arrays

--- Checklists/MainViewController.swift
+++ Checklists/MainViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
+    let groups: [ChecklistGroup] = [ChecklistGroup(title: "Birthdays", imageName: "birthdayspic"), ChecklistGroup(title: "Groceries", imageName: "groceriespic"), ChecklistGroup(title: "To Do", imageName: "To Do pics"), ChecklistGroup(title: "Business Stuff", imageName: "Business pics")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        groups.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let group: ChecklistGroup = groups[indexPath.row]
+         let cell = UITableViewCell()
+        
+        
+        cell.textLabel?.text = group.title
+        return cell
+    }
 }
-
---
+++ Checklists/Models/CheckListsGroup.swift
@@ -0,0 +1,16 @@
+//
+//  CheckListsGroup.swift
+//  Checklists
+//
+//  Created by ASDiMac5 on 18/04/22.
+//
+
+import Foundation
+
+struct ChecklistGroup {
+    var title : String
+    var imageName : String
+    
+}
+
+    
